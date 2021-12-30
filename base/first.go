31ef18f8bceec6d566f690ada4a57955352c36da
李德晟 <2696612575@qq.com>
2021-12-30 23:27:44 +0800

    1. 首次代码提交

---
+++ base/first.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Author : CSR丶WARRIOR
+// Time ： 2021-12-30  23:26
+// TOOLS : GoLand
+// FILENAME : first.go
+// STATEMENT:
+
+func main() {
+	fmt.Println("hello go!")
+}
